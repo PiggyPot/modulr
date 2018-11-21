e4ce5f935c177141388bb191c35db3f826c35356
Phil McClure <phil.mcclure@gmail.com>
2018-11-21 11:44:35 +0000

    Update required elixir ver

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Modulr.MixProject do
       version: "0.1.0",
       description: "Elixir wrapper for the Modulr API",
       package: package(),
-      elixir: "~> 1.6",
+      elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
